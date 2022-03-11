d2a266beb173d7e1dca6042e525239b0496f2aeb
CHAN MATHIEU <pro.matchi1@gmail.com>
2022-03-11 17:14:58 +0100

    TASK 2 OBJECTIVE 1 B.1

--- src/aircraft_manager.cpp
+++ src/aircraft_manager.cpp
@@ -2,27 +2,14 @@
 
 void AircraftManager::add(std::unique_ptr<Aircraft> aircraft)
 {
-    aircrafts.emplace(aircraft->get_flight_num(), std::move(aircraft));
+    aircrafts.emplace_back(std::move(aircraft));
 }
 
 bool AircraftManager::move()
 {
-    for (const auto& [key, value] : aircrafts)
-    {
-        if(value->move())
-        {
-            remove_queue.emplace_back(key);
-        }
-    }
-    for (auto flight : remove_queue) 
-    {
-        remove(flight);
-    }
-    remove_queue.clear();
+    aircrafts.erase(std::remove_if(aircrafts.begin(), 
+                              aircrafts.end(),
+                              [](std::unique_ptr<Aircraft>& aircraft){return aircraft->move();}),
+                              aircrafts.end());
     return false;
-}
-
-void AircraftManager::remove(const std::string flight_num)
-{
-    aircrafts.erase(flight_num);
 }
--- src/aircraft_manager.hpp
+++ src/aircraft_manager.hpp
@@ -7,11 +7,9 @@
 class AircraftManager : public GL::DynamicObject
 {
 private:
-    std::unordered_map<std::string, std::unique_ptr<Aircraft>> aircrafts;
-    std::vector<std::string> remove_queue;
+    std::vector<std::unique_ptr<Aircraft>> aircrafts;
 
 public:
     void add(std::unique_ptr<Aircraft> aircraft);
-    void remove(const std::string flight_num);
     bool move() override;
 };
