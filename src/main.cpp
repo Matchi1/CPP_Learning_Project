fee64ccf135052da4a65bc916853e3165f4a618a
Chan Mathieu <pro.matchi1@gmail.com>
2022-04-15 19:39:26 +0200

    TASK 4 OBJECTIVE 2.2

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include "tower_sim.hpp"
+#include "point.hpp"
+
+void test_generic_point() {
+    Point<2, float> p1;
+    Point<2, float> p2;
+    auto p3 = p1 + p2;
+    p1 += p2;
+    p1 *= 3.; // ou 3.f, ou 3.0 en fonction du type de Point
+    std::cout << p3.x() << std::endl;
+}
 
 int main(int argc, char** argv)
 {
+    test_generic_point();
     TowerSimulation simulation { argc, argv };
     simulation.launch();
 
